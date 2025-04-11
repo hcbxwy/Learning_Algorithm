ccbd530eddf97c232d20396132d233192548f5d7
Silas <silas.codes@hcbxwy.com>
2025-04-11 11:08:13 +0800

    单链表反转 #12

---
+++ src/linkedlist/LinkedList_01.java
@@ -0,0 +1,25 @@
+package linkedlist;
+
+/**
+ * 单链表反转
+ * 示例：1->2->3->null
+ * 反转：null<-1<-2<-3
+ * <a href="https://leetcode.cn/problems/reverse-linked-list/description/">测试网址</a>
+ */
+public class LinkedList_01 {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode next;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+/**
+ * 单链表
+ */
+public class ListNode {
+    Integer value;
+    ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+}
