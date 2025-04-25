e4be67f8b209d503538754cd29316094347716e9
Silas <silas.codes@hcbxwy.com>
2025-04-25 11:08:10 +0800

    使用位运算实现乘法 #23

--- src/other/Issue21_24_BitAddMinusMultiDiv.java
+++ src/other/Issue21_24_BitAddMinusMultiDiv.java
@@ -36,10 +36,27 @@ public class Issue21_24_BitAddMinusMultiDiv {
         return add(a, negNum(b));
     }
 
+    /**
+     * 乘法：跟十进制的乘法一样，只是转成二进制相乘
+     */
+    public static int multi(int a, int b) {
+        int ans = 0;
+        while (b != 0) {
+            if ((b & 1) != 0) {
+                ans = add(ans, a);
+            }
+            a <<= 1;
+            b >>>= 1;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         // 测试加法
         // System.out.println(add(100, 99));
         // 测试减法
-        System.out.println(minus(10, -10));
+        // System.out.println(minus(10, -10));
+        // 测试乘法
+        System.out.println(multi(9, 7));
     }
 }
