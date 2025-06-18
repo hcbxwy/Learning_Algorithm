7075add5046ac4a6425c6d9d800e0ea0794fbb5c
Silas <silas.codes@hcbxwy.com>
2025-06-18 11:14:54 +0800

    实现一个最小栈 #48

---
+++ src/other/Issue48_minStack.java
@@ -0,0 +1,50 @@
+package other;
+
+import java.util.Stack;
+
+/**
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p></p>
+ * 实现 MinStack 类:
+ * MinStack() 初始化堆栈对象。
+ * void push(int val) 将元素val推入堆栈。
+ * void pop() 删除堆栈顶部的元素。
+ * int top() 获取堆栈顶部的元素。
+ * int getMin() 获取堆栈中的最小元素。
+ * <a href="https://leetcode.cn/problems/min-stack/">测试链接</a>
+ */
+public class Issue48_minStack {
+
+    static class MinStack {
+        public final Stack<Integer> data;
+        public final Stack<Integer> min;
+
+        public MinStack() {
+            data = new Stack<>();
+            min = new Stack<>();
+        }
+
+        public void push(int val) {
+            data.push(val);
+            if (min.isEmpty() || val <= min.peek()) {
+                min.push(val);
+            } else {
+                // 当前值大于最小栈的栈顶时，把栈顶元素再压入，确保最小栈和数据栈大小一致
+                min.push(min.peek());
+            }
+        }
+
+        public void pop() {
+            data.pop();
+            min.pop();
+        }
+
+        public int top() {
+            return data.peek();
+        }
+
+        public int getMin() {
+            return min.peek();
+        }
+    }
+}
