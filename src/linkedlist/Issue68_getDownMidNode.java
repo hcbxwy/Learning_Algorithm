b985b4d393587f2a269d127af237ab4429f4b742
Silas <silas.codes@hcbxwy.com>
2025-07-03 11:15:33 +0800

    输入链表头节点，奇数长度返回中点，偶数长度返回下中点 #68

--- src/linkedlist/Issue68_getDownMidNode.java
+++ src/linkedlist/Issue68_getDownMidNode.java
@@ -1,31 +1,31 @@
 package linkedlist;
 
 /**
- * 输入链表头节点，奇数长度返回中点，偶数长度返回上中点
+ * 输入链表头节点，奇数长度返回中点，偶数长度返回下中点
  *
  * @author hcb
- * @since 2025/7/3 10:20
+ * @since 2025/7/3 10:38
  */
-public class Issue67_getUpMidNode {
+public class Issue68_getDownMidNode {
 
     public static void main(String[] args) {
         Node head = new Node(1);
-        // head.next = new Node(2);
-        // head.next.next = new Node(3);
-        // head.next.next.next = new Node(4);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
         // head.next.next.next.next = new Node(5);
         // head.next.next.next.next.next = new Node(6);
-        Node upMidNode = getUpMidNode(head);
-        System.out.println(upMidNode != null ? upMidNode.val : null);
+        Node downMidNode = getDownMidNode(head);
+        System.out.println(downMidNode != null ? downMidNode.val : null);
     }
 
-    private static Node getUpMidNode(Node head) {
+    private static Node getDownMidNode(Node head) {
         if (head == null) {
             return null;
         }
         Node slow = head;
         Node fast = head;
-        while (fast.next != null && fast.next.next != null) {
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
@@ -36,7 +36,7 @@ public class Issue67_getUpMidNode {
         private final int val;
         private Node next;
 
-        public Node(int val) {
+        private Node(int val) {
             this.val = val;
         }
     }
