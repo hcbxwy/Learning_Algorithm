1b870aabddad3770e7b6bf7351772b2f8404b981
Silas <silas.codes@hcbxwy.com>
2025-07-14 11:19:36 +0800

    用非递归方式实现二叉树的前中后序 #76

---
+++ src/tree/Issue76_UnRecursiveTraversalBT.java
@@ -0,0 +1,89 @@
+package tree;
+
+import java.util.Stack;
+
+/**
+ * 用非递归方式实现二叉树的前中后序
+ *
+ * @author hcb
+ * @since 2025/7/14 09:40
+ */
+public class Issue76_UnRecursiveTraversalBT {
+
+    public static void main(String[] args) {
+        TreeNode head = new TreeNode(0);
+        head.left = new TreeNode(1);
+        head.right = new TreeNode(2);
+        head.left.left = new TreeNode(3);
+        head.left.right = new TreeNode(4);
+        head.right.left = new TreeNode(5);
+        head.right.right = new TreeNode(6);
+
+        preOrder(head);
+        inOrder(head);
+        posOrder(head);
+    }
+
+    // 使用一个栈，子节点入栈先右再左
+    public static void preOrder(TreeNode head) {
+        System.out.print("前序遍历: ");
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(head);
+            while (!stack.isEmpty()) {
+                TreeNode cur = stack.pop();
+                System.out.print(cur.val + " ");
+                // 先入右，再入左
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    // 当前节点不为空则左子节点入栈，为空则从栈中弹出一个，并把指针指向右子节点
+    public static void inOrder(TreeNode head) {
+        System.out.print("中序遍历: ");
+        Stack<TreeNode> stack = new Stack<>();
+        while (head != null || !stack.isEmpty()) {
+            if (head != null) {
+                stack.push(head);
+                head = head.left;
+            } else {
+                head = stack.pop();
+                System.out.print(head.val + " ");
+                head = head.right;
+            }
+        }
+        System.out.println();
+    }
+
+    // 使用两个栈，从栈1弹出后，先入栈2，然后左右节点进栈1
+    public static void posOrder(TreeNode head) {
+        System.out.print("后序遍历: ");
+        if (head != null) {
+            Stack<TreeNode> stack1 = new Stack<>();
+            Stack<TreeNode> stack2 = new Stack<>();
+            stack1.push(head);
+            while (!stack1.isEmpty()) {
+                TreeNode cur = stack1.pop();
+                stack2.push(cur);
+                if (cur.left != null) {
+                    stack1.push(cur.left);
+                }
+                if (cur.right != null) {
+                    stack1.push(cur.right);
+                }
+            }
+            while (!stack2.isEmpty()) {
+                System.out.print(stack2.pop().val + " ");
+            }
+        }
+
+        System.out.println();
+    }
+}
